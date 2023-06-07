66984a9dd3adaf7abf29032a0580274ffb5a73cd
OpenByte <development.openbyte@gmail.com>
2023-06-07 13:11:28 +0200

    Cargo fmt

--- examples/cli.rs
+++ examples/cli.rs
@@ -2,13 +2,10 @@ use std::env;
 
 use doi2bib::Doi2Bib;
 
-#[tokio::main(flavor="current_thread")]
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
     // TODO: use iter_intersperse once stable
-    let arg = env::args()
-        .skip(1)
-        .collect::<Vec<_>>()
-        .join(" ");
+    let arg = env::args().skip(1).collect::<Vec<_>>().join(" ");
     let resolver = Doi2Bib::new().unwrap();
     let bibtex = resolver.resolve(&arg).await.unwrap().unwrap();
     println!("{}", bibtex);
